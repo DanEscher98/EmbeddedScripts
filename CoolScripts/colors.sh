32d5f84fd752d8643c9ceaa297144f62ef511ca3
DanEscher98 <danyiel5978@gmail.com>
2021-11-19 01:58:03 -0600

    new file:   CoolScripts/colors.sh 	new file:   Experiments/amongus.sh

---
+++ CoolScripts/colors.sh
@@ -0,0 +1,27 @@
+# ----------------------------------
+# Colors
+# ----------------------------------
+NOCOLOR='\e[0m'
+RED='\e[0;31m'
+GREEN='\e[0;32m'
+ORANGE='\e[0;33m'
+BLUE='\e[0;34m'
+PURPLE='\e[0;35m'
+CYAN='\e[0;36m'
+LIGHTGRAY='\e[0;37m'
+DARKGRAY='\e[1;30m'
+LIGHTRED='\e[1;31m'
+LIGHTGREEN='\e[1;32m'
+YELLOW='\e[1;33m'
+LIGHTBLUE='\e[1;34m'
+LIGHTPURPLE='\e[1;35m'
+LIGHTCYAN='\e[1;36m'
+WHITE='\e[1;37m'
+BCBLACK='\e[0;40'
+BCRED='\e[0;41m'
+BCGREEN='\e[0;42m'
+BCORANGE='\e[0;43m'
+BCBLUE='\e[0;44m'
+BCPURPLE='\e[0;45m'
+BCCYAN='\e[0;46m'
+# echo -e $color
---
+++ Experiments/amongus.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+NOCOLOR='\e[0m'
+RED='\e[0;31m'
+amogus="$REDඞ $NOCOLOR"
+
+echo -e ". 　　　。　　　　•　 　ﾟ　　。 　　.
+　。　　　.　　　　　  。　   　.
+.　　 。　　　· $amogus        。   . 　　•
+ﾟ　　 Red was not An Impostor.　   。　     .
+　　　 1 Impostor remains 　 　. 　   。  ·
+ﾟ　　　.　　　. ,　　　.　 ·    .  。　' "
