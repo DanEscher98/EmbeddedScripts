9792d1ae4051d3292fdfcae19d463d1715959bd0
Daniel Colin <danyiel5978@gmail.com>
2021-12-14 23:33:23 -0600

    modified:   Workflows/InitialSetup/initial_setup.sh

--- Workflows/InitialSetup/initial_setup.sh
+++ Workflows/InitialSetup/initial_setup.sh
@@ -1,7 +1,44 @@
 #!/usr/bin/bash
 
+# 1. Package managers and essentials
+#	- apt, flatpak, fish, commands
+# 2. Development tools
+#	- nvim, coc, language servers, compilers
+#	- gh, git
+# 3. Extras
+
+# Initialization
+mkdir ~/Working ~/Packages
+
+# PACKAGE MANAGERS
 sudo apt update && sudo apt upgrade
-# Basic and useful commands
+sudo add-apt-repository ppa:appimagelauncher-team/stable
+sudo apt-get update
+sudo apt-get install appimagelauncher
+
+# The Fish shell
+sudo apt install fish
+chsh -s /usr/bin/fish
+mkdir ~/.config/fish
+mkdir ~/.config/fish/functions
+echo 'set fish_greeting
+cd ~/Working
+set -xg EDITOR nvim
+set -gx PAGER less
+fish_vi_key_bindings
+source /usr/share/autojump/autojump.fish
+alias bat="batcat"
+alias tress="tree|less"
+alias cls="clear && ls"' > ~/.config/fish/config.fish
+ln -s ~/.config/fish/config.fish ~/.fishrc
+curl -L https://get.oh-my.fish | fish
+omf install pisces
+if [ -d Fish ]; then
+	mv Fish/*.fish ~/.config/nvim/functions
+fi
+
+
+# BASIC AND USEFUL COMMANDS
 sudo apt install curl build-essential
 sudo apt -y install neofetch trash-cli
 sudo apt install fonts-powerline
@@ -9,14 +46,31 @@ sudo apt -y install gawk mplayer
 sudo mv trans ~/usr/local/bin/
 sudo apt install autojump
 
-# Configure package mangers
-sudo add-apt-repository ppa:appimagelauncher-team/stable
-sudo apt-get update
-sudo apt-get install appimagelauncher
+# Configuring nvim
+sudo apt install neovim
+mkdir ~/.config/nvim
+mkdir ~/.config/nvim/autoload
+mkdir ~/.config/nvim/autoload/plugged
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+if [ -d nvim ]; then
+	mv nvim/coc-settings.json nvim/init.vim nvim/plugins.vim
+	\ ~/.config/nvim/
+fi
 
 # Developing tools
-sudo apt -y install clang clangd make
+echo "deb [arch=$(dpkg --print-architecture)" \
+	"signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg]" \
+	"https://cli.github.com/packages stable main" \
+	| sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	| sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+
+sudo apt install gh
+sudo apt -y install clang --install-suggests
+sudo apt install clangd make valgrind
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Autoformaters
@@ -29,41 +83,13 @@ npm install -g remark-cli
 go get -u mvdan.cc/sh/cmd/shfmt
 go get -u github.com/klauspost/asmfmt/cmd/asmfmt
 
-# Configuring nvim
-sudo apt install nvim
-mkdir ~/.config/nvim/autoload
-mkdir ~/.config/nvim
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	if [ -d nvim ]; then
-		mv nvim/coc-settings.json nvim/init.vim nvim/plugins.vim
-		\ ~/.config/nvim/
-	fi
-
 # Language Servers and NPM
-
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt install nodejs
 npm i -g bash-language-server
 rustup component add rls rust-analysis rust-src
 cargo install --git https://github.com/bergercookie/asm-lsp
 
-# The Fish shell
-sudo apt install fish
-chsh -s /usr/bin/fish
-mkdir ~/.config/fish
-mkdir ~/.config/fish/functions
-touch ~/.config/fish/config.fish
-if [ -d Fish ]; then
-	mv Fish/*.fish ~/.config/nvim/functions
-fi
-ln -s ~/.config/fish/config.fish ~/.fishrc
-curl -L https://get.oh-my.fish | fish
-omf install pisces
-omf install harleen
-omf theme harleen
-echo "source /usr/share/autojump/autojump.fish" >> ~/.fishrc
-
 # C Drive in WSL
-mkdir ~/MyWindows
+# mkdir ~/MyWindows
 # ls -s /mnt/c/User/$name/Documents/Programs ~/MyWindows
