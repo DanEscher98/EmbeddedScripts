10b740a01fc85233b2cf26d20cfc31dc47582855
Danyiel Colin <danyiel5978@gmail.com>
2022-05-22 03:25:24 -0500

    modified:   install.sh

--- PyFish/install.sh
+++ PyFish/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+# iVV Z52mQF6q4jNSB L321jK0f1JH33
+# iVV Bf2Qhm3PqM7nR L321jK0f1JH33
 # cntlang.py
 file_url="https://raw.githubusercontent.com/DanEscher98/Scripts/main/PyFish/cntlang.py?_sm_au_=iVVBf2Qhm3PqM7nRL321jK0f1JH33"
 out_dir="$HOME/.local/bin"
