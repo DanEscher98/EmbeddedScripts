dd4f8250dc9500b42751072b492a4e7128019c5f
Danyiel Colin <danyiel5978@gmail.com>
2022-07-15 02:27:26 -0500

    modified:   Languages/golang-work.sh

--- Scripts/Workflows/Languages/golang-work.sh
+++ Scripts/Workflows/Languages/golang-work.sh
@@ -1,1 +1,3 @@
 sudo apt install golang\ngo install golang.org/x/tools/gopls@latest
+go install github.com/traefik/yaegi/cmd/yaegi@latst
+curl -sfL https://raw.githubusercontent.com/traefik/yaegi/master/install.sh | bash -s -- -b $GOPATH/bin v0.9.0
